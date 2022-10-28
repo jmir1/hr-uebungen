79eaaa67939ec66683b78834dd5dadd82957e14f
Merle Hoffmann <hoffmannmerle@web.de>
2022-10-28 20:40:56 +0200

    add: finished exercise02 task1.1

--- blatt02/HoffmannMiramonPress/simple/simple.c
+++ blatt02/HoffmannMiramonPress/simple/simple.c
@@ -5,30 +5,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int *mistake1(void) {
+    int *buf_alloc = (int *)malloc(sizeof(int) * 6);
     int buf[] = {1, 1, 2, 3, 4, 5};
-    return buf;
+    memcpy(buf_alloc, buf, sizeof(int) * 6);
+    return buf_alloc;
 }
 
 int *mistake2(void) {
-    int *buf = malloc(sizeof(char) * 4);
-    buf[2] = 2;
+    int *buf = malloc(sizeof(int) * 4);
+    buf[1] = 2;
     return buf;
 }
 
 int *mistake3(void) {
     /* In dieser Funktion darf kein Speicher direkt allokiert werden. */
-    int mistake2_ = 0;
-    int *buf = (int *)&mistake2;
+    int *mistake2_ = mistake2();
+    int *buf = mistake2_;
     buf[0] = 3;
     return buf;
 }
 
 int *mistake4(void) {
-    int *buf = malloc(sizeof(char) * 4);
-    buf[4] = 4;
-    free(buf);
+    int *buf = malloc(sizeof(int) * 4);
+    buf[0] = 4;
     return buf;
 }
 
@@ -43,7 +45,9 @@ int main(void) {
 
     /* mhh muss hier noch etwas gefreed werden? */
     /* Fügen sie hier die korrekten aufrufe von free() ein */
-    free(p[1]); /* welcher Pointer war das doch gleich?, TODO: Fixme... :-) */
-
+    free(p[0] - 1);
+    free(p[1] - 1); /* welcher Pointer war das doch gleich?, TODO: Fixme... :-) */
+    free(p[2]);
+    free(p[3]);
     return 0;
 }
