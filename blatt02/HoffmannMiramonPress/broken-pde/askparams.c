5b9d9f75935c24ee5898e21bf06c39649865adbd
Merle Hoffmann <47038664+dasmerlon@users.noreply.github.com>
2022-10-28 23:09:07 +0200

    Exercise02 merle (#2)
    * rebase
    
    * refactor: Makefile update
    
    * add: finished exercise02 task1.1
    
    * add: finished exercise02 task1.2

--- blatt02/HoffmannMiramonPress/broken-pde/askparams.c
+++ blatt02/HoffmannMiramonPress/broken-pde/askparams.c
@@ -75,137 +75,107 @@
 #include "partdiff-seq.h"
 #include <string.h>
 
-void
-AskParams (int *method,
-	   int *interlines,
-	   int *func,
-	   int *termination,
-	   double *term_precision, int *term_iteration, int argC, char **argV)
-{
-  printf ("\n");
-  printf ("============================================================\n");
-  printf ("Program for calculation of partial differential equations.  \n");
-  printf ("============================================================\n");
-  printf ("(c) Dr. Thomas Ludwig, TU M\"unchen.\n");
-  printf ("    Thomas A. Zochler, TU M\"unchen.\n");
-  printf ("    Andreas C. Schmidt, TU M\"unchen.\n");
-  printf ("============================================================\n");
+void AskParams(int *method, int *interlines, int *func, int *termination,
+               double *term_precision, int *term_iteration, int argC, char **argV) {
+    printf("\n");
+    printf("============================================================\n");
+    printf("Program for calculation of partial differential equations.  \n");
+    printf("============================================================\n");
+    printf("(c) Dr. Thomas Ludwig, TU M\"unchen.\n");
+    printf("    Thomas A. Zochler, TU M\"unchen.\n");
+    printf("    Andreas C. Schmidt, TU M\"unchen.\n");
+    printf("============================================================\n");
 
-  if (argC < 2)
-    {
-      /* ----------------------------------------------- */
-      /* Get input: method, interlines, func, precision. */
-      /* ----------------------------------------------- */
-      do
-	{
-	  printf ("\n");
-	  printf ("Select calculation method:\n");
-	  printf ("  %1d: Gauss-Seidel.\n", GAUSS_SEIDEL);
-	  printf ("  %1d: Jacobi.\n", JACOBI);
-	  printf ("method> ");
-	  fflush (stdout);
-	  scanf ("%d", method);
-	}
-      while ((*method < GAUSS_SEIDEL) || (*method > JACOBI));
-      do
-	{
-	  printf ("\n");
-	  printf ("Matrixsize = Interlines*8+9\n");
-	  printf ("Interlines> ");
-	  fflush (stdout);
-	  scanf ("%d", interlines);
-	}
-      while ((*interlines < 0) || (*interlines > 1000));
-      do
-	{
-	  printf ("\n");
-	  printf ("Select interference function:\n");
-	  printf (" %1d: f(x,y)=0.\n", FUNC_1);
-	  printf (" %1d: f(x,y)=2pi^2*sin(pi*x)sin(pi*y).\n", FUNC_2);
-	  printf ("interference function> ");
-	  fflush (stdout);
-	  scanf ("%d", func);
-	}
-      while ((*func < FUNC_1) || (*func > FUNC_2));
-      do
-	{
-	  printf ("\n");
-	  printf ("Select termination:\n");
-	  printf (" %1d: sufficient precision.\n", TERM_1);
-	  printf (" %1d: number of iterations.\n", TERM_2);
-	  printf ("termination> ");
-	  fflush (stdout);
-	  scanf ("%d", termination);
-	}
-      while ((*termination < TERM_1) || (*termination > TERM_2));
-      if (*termination == TERM_1)
-	{
-	  do
-	    {
-	      printf ("\n");
-	      printf ("Select precision:\n");
-	      printf ("  Range: 1e-4 .. 1e-20.\n");
-	      printf ("precision> ");
-	      fflush (stdout);
-	      scanf ("%lf", term_precision);
-	    }
-	  while ((*term_precision < 1e-20) || (*term_precision > 1e-4));
-	  *term_iteration = MAX_ITERATION;
-	}
-      if (*termination == TERM_2)
-	{
-	  do
-	    {
-	      printf ("\n");
-	      printf ("Select number of iterations:\n");
-	      printf ("  Range: 1 .. %d.\n", MAX_ITERATION);
-	      printf ("Iterations> ");
-	      fflush (stdout);
-	      scanf ("%d", term_iteration);
-	    }
-	  while ((*term_iteration < 1) || (*term_iteration > MAX_ITERATION));
-	  *term_precision = 0;
-	}
-    }
-  else
-    {
-      if (strcmp ("help", (char *) argV[1]) == 0
-	  || strcmp ("-h", (char *) argV[1]) == 0
-	  || strcmp ("-?", (char *) argV[1]) == 0
-	  || strcmp ("?", (char *) argV[1]) == 0 || argC < 6)
-	{
-	  printf ("\nUsage:\n");
-	  printf
-	    ("partdiff [num] [method] [lines] [func] [term] [prec/iter]\n");
-	  printf ("  - num:    number of process/threads to use\n");
-	  printf ("  - method: %1d: Gauss-Seidel.\n", GAUSS_SEIDEL);
-	  printf ("            %1d: Jacobi.\n", JACOBI);
-	  printf
-	    ("  - lines:  (lines=interlines) matrixsize = interlines*8+9\n");
-	  printf ("  - func:   %1d: f(x,y)=0.\n", FUNC_1);
-	  printf ("            %1d: f(x,y)=2pi^2*sin(pi*x)sin(pi*y).\n",
-		  FUNC_2);
-	  printf ("  - term:   %1d: sufficient precision.\n", TERM_1);
-	  printf ("            %1d: number of iterations.\n", TERM_2);
-	  printf ("  - prec/iter: depending on term:\n");
-	  printf ("            precision:  Range: 1e-4 .. 1e-20.\n");
-	  printf ("            iterations: Range: 1 .. %d.\n", MAX_ITERATION);
-	  printf ("\n");
-	  exit (0);
-	}
-      sscanf (argV[2], "%d", method);
-      sscanf (argV[3], "%d", interlines);
-      sscanf (argV[4], "%d", func);
-      sscanf (argV[5], "%d", termination);
-      if (*termination == 1)
-	{
-	  sscanf (argV[6], "%lf", term_precision);
-	  *term_iteration = MAX_ITERATION;
-	}
-      else
-	{
-	  sscanf (argV[6], "%d", term_iteration);
-	  *term_precision = 0;
-	}
+    if (argC < 2) {
+        /* ----------------------------------------------- */
+        /* Get input: method, interlines, func, precision. */
+        /* ----------------------------------------------- */
+        do {
+            printf("\n");
+            printf("Select calculation method:\n");
+            printf("  %1d: Gauss-Seidel.\n", GAUSS_SEIDEL);
+            printf("  %1d: Jacobi.\n", JACOBI);
+            printf("method> ");
+            fflush(stdout);
+            scanf("%d", method);
+        } while ((*method < GAUSS_SEIDEL) || (*method > JACOBI));
+        do {
+            printf("\n");
+            printf("Matrixsize = Interlines*8+9\n");
+            printf("Interlines> ");
+            fflush(stdout);
+            scanf("%d", interlines);
+        } while ((*interlines < 0) || (*interlines > 1000));
+        do {
+            printf("\n");
+            printf("Select interference function:\n");
+            printf(" %1d: f(x,y)=0.\n", FUNC_1);
+            printf(" %1d: f(x,y)=2pi^2*sin(pi*x)sin(pi*y).\n", FUNC_2);
+            printf("interference function> ");
+            fflush(stdout);
+            scanf("%d", func);
+        } while ((*func < FUNC_1) || (*func > FUNC_2));
+        do {
+            printf("\n");
+            printf("Select termination:\n");
+            printf(" %1d: sufficient precision.\n", TERM_1);
+            printf(" %1d: number of iterations.\n", TERM_2);
+            printf("termination> ");
+            fflush(stdout);
+            scanf("%d", termination);
+        } while ((*termination < TERM_1) || (*termination > TERM_2));
+        if (*termination == TERM_1) {
+            do {
+                printf("\n");
+                printf("Select precision:\n");
+                printf("  Range: 1e-4 .. 1e-20.\n");
+                printf("precision> ");
+                fflush(stdout);
+                scanf("%lf", term_precision);
+            } while ((*term_precision < 1e-20) || (*term_precision > 1e-4));
+            *term_iteration = MAX_ITERATION;
+        }
+        if (*termination == TERM_2) {
+            do {
+                printf("\n");
+                printf("Select number of iterations:\n");
+                printf("  Range: 1 .. %d.\n", MAX_ITERATION);
+                printf("Iterations> ");
+                fflush(stdout);
+                scanf("%d", term_iteration);
+            } while ((*term_iteration < 1) || (*term_iteration > MAX_ITERATION));
+            *term_precision = 0;
+        }
+    } else {
+        if (strcmp("help", (char *)argV[1]) == 0 ||
+            strcmp("-h", (char *)argV[1]) == 0 || strcmp("-?", (char *)argV[1]) == 0 ||
+            strcmp("?", (char *)argV[1]) == 0 || argC < 6) {
+            printf("\nUsage:\n");
+            printf("partdiff [num] [method] [lines] [func] [term] [prec/iter]\n");
+            printf("  - num:    number of process/threads to use\n");
+            printf("  - method: %1d: Gauss-Seidel.\n", GAUSS_SEIDEL);
+            printf("            %1d: Jacobi.\n", JACOBI);
+            printf("  - lines:  (lines=interlines) matrixsize = interlines*8+9\n");
+            printf("  - func:   %1d: f(x,y)=0.\n", FUNC_1);
+            printf("            %1d: f(x,y)=2pi^2*sin(pi*x)sin(pi*y).\n", FUNC_2);
+            printf("  - term:   %1d: sufficient precision.\n", TERM_1);
+            printf("            %1d: number of iterations.\n", TERM_2);
+            printf("  - prec/iter: depending on term:\n");
+            printf("            precision:  Range: 1e-4 .. 1e-20.\n");
+            printf("            iterations: Range: 1 .. %d.\n", MAX_ITERATION);
+            printf("\n");
+            exit(0);
+        }
+        sscanf(argV[2], "%d", method);
+        sscanf(argV[3], "%d", interlines);
+        sscanf(argV[4], "%d", func);
+        sscanf(argV[5], "%d", termination);
+        if (*termination == 1) {
+            sscanf(argV[6], "%lf", term_precision);
+            *term_iteration = MAX_ITERATION;
+        } else {
+            sscanf(argV[6], "%d", term_iteration);
+            *term_precision = 0;
+        }
     }
 }
--- blatt02/HoffmannMiramonPress/simple/simple.c
+++ blatt02/HoffmannMiramonPress/simple/simple.c
@@ -5,58 +5,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int *
-mistake1 (void)
-{
-  int *buf = malloc(sizeof (int) * 2);
-  buf[1] = 1;
-  return buf;
+int *mistake1(void) {
+    int *buf_alloc = (int *)malloc(sizeof(int) * 6);
+    int buf[] = {1, 1, 2, 3, 4, 5};
+    memcpy(buf_alloc, buf, sizeof(int) * 6);
+    return buf_alloc;
 }
 
-int *
-mistake2 (void)
-{
-  int *buf = malloc (sizeof (int) * 2);
-  buf[1] = 2;
-  return buf;
+int *mistake2(void) {
+    int *buf = malloc(sizeof(int) * 4);
+    buf[1] = 2;
+    return buf;
 }
 
-int *
-mistake3 (void)
-{
-  /* In dieser Funktion darf kein Speicher direkt allokiert werden. */
-  int *mistake2_ = malloc(sizeof (int));
-  int *buf = (int *) mistake2_;
-  buf[0] = 3;
-  return buf;
+int *mistake3(void) {
+    /* In dieser Funktion darf kein Speicher direkt allokiert werden. */
+    int *mistake2_ = mistake2();
+    int *buf = mistake2_;
+    buf[0] = 3;
+    return buf;
 }
 
-int *
-mistake4 (void)
-{
-  int *buf = malloc (sizeof (int));
-  buf[0] = 4;
-  return buf;
+int *mistake4(void) {
+    int *buf = malloc(sizeof(int) * 4);
+    buf[0] = 4;
+    return buf;
 }
 
-int
-main (void)
-{
-  /* Modifizieren Sie diese Zeile nicht! */
-  int *p[4] = { &mistake1 ()[1], &mistake2 ()[1], mistake3 (), mistake4 () };
+int main(void) {
+    /* Modifizieren Sie diese Zeile nicht! */
+    int *p[4] = {&mistake1()[1], &mistake2()[1], mistake3(), mistake4()};
 
-  printf ("1 - %d\n", *p[0]);
-  printf ("2 - %d\n", *p[1]);
-  printf ("3 - %d\n", *p[2]);
-  printf ("4 - %d\n", *p[3]);
+    printf("1 - %d\n", *p[0]);
+    printf("2 - %d\n", *p[1]);
+    printf("3 - %d\n", *p[2]);
+    printf("4 - %d\n", *p[3]);
 
-  /* mhh muss hier noch etwas gefreed werden? */
-  /* Fügen sie hier die korrekten aufrufe von free() ein */
-  free (&p[0][-1]);
-  free (&p[1][-1]);			/* welcher Pointer war das doch gleich?, TODO: Fixme... :-) */
-  free (p[2]);
-  free (p[3]);
-
-  return 0;
+    /* mhh muss hier noch etwas gefreed werden? */
+    /* Fügen sie hier die korrekten aufrufe von free() ein */
+    free(p[0] - 1);
+    free(p[1] - 1); /* welcher Pointer war das doch gleich?, TODO: Fixme... :-) */
+    free(p[2]);
+    free(p[3]);
+    return 0;
 }
